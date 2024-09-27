4c967c2bc30748ac96ce79e08bb9950b787c4f66
jakeevans <ejake370@gmail.com>
2024-09-26 18:09:08 -0600

    Complete Part 1 (Milestone 1)

---
+++ tweeter-web/src/components/authentication/AuthenticationFields.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+type Props = {
+  fn: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+  setAlias: React.Dispatch<React.SetStateAction<string>>;
+  setPassword: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export default function AuthenticationFields({
+  fn,
+  setAlias,
+  setPassword,
+}: Props) {
+  return (
+    <>
+      <div className="form-floating">
+        <input
+          type="text"
+          className="form-control"
+          size={50}
+          id="aliasInput"
+          placeholder="name@example.com"
+          onKeyDown={fn}
+          onChange={(event) => setAlias(event.target.value)}
+        />
+        <label htmlFor="aliasInput">Alias</label>
+      </div>
+      <div className="form-floating mb-3">
+        <input
+          type="password"
+          className="form-control bottom"
+          id="passwordInput"
+          placeholder="Password"
+          onKeyDown={fn}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <label htmlFor="passwordInput">Password</label>
+      </div>
+    </>
+  );
+}
